459d017ccfe99f8e62029ab30766c3f433663bc7
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2016-09-19 21:10:35 -0500

    SubModulo Solicitud

--- public/app/controllers/solicitudController.js
+++ public/app/controllers/solicitudController.js
@@ -9,4 +9,39 @@ app.controller('solicitudController', function($scope, $http, API_URL) {
         });
     };
 
+    $scope.toggle = function(modalstate, id) {
+        $scope.modalstate = modalstate;
+
+        switch (modalstate) {
+            case 'add':
+
+                /*$http.get(API_URL + 'cargo/lastId').success(function(response){
+
+                    $scope.idcargo = response.lastId;
+                    $scope.form_title = "Ingresar nuevo Cargo";
+                    $scope.nombrecargo = '';
+                    $('#modalActionCargo').modal('show');
+                });*/
+
+                $('#modalIngSolicitud').modal('show');
+
+                break;
+            case 'edit':
+                $scope.form_title = "Editar Cargo";
+                $scope.id = id;
+
+                $http.get(API_URL + 'cargo/' + id).success(function(response) {
+                    $scope.idcargo = (response.idcargo).trim();
+                    $scope.nombrecargo = (response.nombrecargo).trim();
+                    $('#modalActionCargo').modal('show');
+                });
+
+                break;
+            default:
+                break;
+        }
+
+
+    };
+
 });
--- resources/views/solicitud/solicitud.php
+++ resources/views/solicitud/solicitud.php
@@ -17,6 +17,7 @@
     <body>
 
         <div class="col-xs-12" ng-controller="solicitudController" style="margin-top: 2%;">
+
             <div class="col-xs-12">
                 <div class="col-sm-6 col-xs-12">
                     <div class="form-group has-feedback">
@@ -65,6 +66,139 @@
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+            <div class="modal fade" tabindex="-1" role="dialog" id="modalIngSolicitud">
+                <div class="modal-dialog" role="document" style="width: 60%;">
+                    <div class="modal-content">
+                        <div class="modal-header modal-header-primary">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Ingresar Solicitud</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form class="form-horizontal" name="formCargo" novalidate="">
+
+                                <div class="row">
+                                    <div class="col-sm-6 col-xs-12 form-group">
+                                        <label for="t_no_solicitud" class="col-sm-4 col-xs-12 control-label">Nro. Solicitud:</label>
+                                        <div class="col-sm-8 col-xs-12">
+                                            <input type="text" class="form-control" name="t_no_solicitud" id="t_no_solicitud" ng-model="t_no_solicitud" disabled>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-xs-12 form-group">
+                                        <label for="t_fecha_ingreso" class="col-sm-4 col-xs-12 control-label">Fecha Ingreso:</label>
+                                        <div class="col-sm-8 col-xs-12">
+                                            <input type="text" class="form-control" name="t_fecha_ingreso" id="t_fecha_ingreso" ng-model="t_fecha_ingreso">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row" style="padding: 2%; margin-top: -15px !important;">
+                                    <filedset>
+                                        <legend>Datos de Cliente</legend>
+
+                                        <div class="col-xs-12" style="padding: 0;">
+                                            <div class="col-sm-6 col-xs-12">
+                                                <label for="t_doc_id" class="col-sm-4 col-xs-12 control-label">Documento ID:</label>
+                                                <div class="col-sm-8 col-xs-12">
+                                                    <input type="text" class="form-control" name="t_doc_id" id="t_fecha_ingreso" ng-model="t_doc_id">
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 col-xs-12"></div>
+                                        </div>
+
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_apellidos" class="col-sm-4 col-xs-12 control-label">Apellidos:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_apellidos" id="t_apellidos" ng-model="t_apellidos">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_nombres" class="col-sm-4 col-xs-12 control-label">Nombre(s):</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_nombres" id="t_nombres" ng-model="t_nombres">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_telf_principal" class="col-sm-4 col-xs-12 control-label" style="padding: 5px 0 5px 0;">Teléf. Principal:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_telf_principal" id="t_telf_principal" ng-model="t_telf_principal">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_telf_secundario" class="col-sm-4 col-xs-12 control-label" style="padding: 5px 0 5px 0;">Teléf. Secundario:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_telf_secundario" id="t_telf_secundario" ng-model="t_telf_secundario">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_celular" class="col-sm-4 col-xs-12 control-label">Celular:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_celular" id="t_celular" ng-model="t_celular">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_direccion" class="col-sm-4 col-xs-12 control-label">Dirección:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_direccion" id="t_direccion" ng-model="t_direccion">
+                                            </div>
+                                        </div>
+                                    </filedset>
+                                </div>
+
+                                <div class="row" style="padding: 2%;">
+                                    <fieldset>
+                                        <legend>Datos del Trabajo</legend>
+                                        <div class="col-sm-6 col-xs-12">
+                                            <label for="t_telf_principal_emp" class="col-sm-4 col-xs-12 control-label" style="padding: 5px 0 5px 0;">Teléf. Principal:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_telf_principal_emp" id="t_telf_principal_emp" ng-model="t_telf_principal_emp">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12">
+                                            <label for="t_telf_secundario_emp" class="col-sm-4 col-xs-12 control-label" style="padding: 5px 0 5px 0;">Teléf. Secundario:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_telf_secundario_emp" id="t_telf_secundario_emp" ng-model="t_telf_secundario_emp">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12" style="margin-top: 5px;">
+                                            <label for="t_direccion_emp" class="col-sm-4 col-xs-12 control-label">Dirección:</label>
+                                            <div class="col-sm-8 col-xs-12">
+                                                <input type="text" class="form-control" name="t_direccion_emp" id="t_direccion_emp" ng-model="t_direccion_emp">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+
+                                <!--<div class="form-group">
+                                    <label for="t_codigo_cargo" class="col-sm-4 control-label">Código Cargo:</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" name="idcargo" id="idcargo" ng-model="idcargo" placeholder="" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group error">
+                                    <label for="t_name_cargo" class="col-sm-4 control-label">Nombre del Cargo:</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control" name="nombrecargo" id="nombrecargo" ng-model="nombrecargo" placeholder=""
+                                               ng-required="true" ng-maxlength="16">
+                                        <span class="help-block error"
+                                              ng-show="formCargo.nombrecargo.$invalid && formCargo.nombrecargo.$touched">El nombre del Cargo es requerido</span>
+                                        <span class="help-block error"
+                                              ng-show="formCargo.nombrecargo.$invalid && formCargo.nombrecargo.$error.maxlength">La longitud máxima es de 16 caracteres</span>
+                                    </div>
+                                </div>-->
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">
+                                Cancelar <span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>
+                            </button>
+                            <button type="button" class="btn btn-success" id="btn-save" ng-click="save(modalstate, id)" ng-disabled="formCargo.$invalid">
+                                Guardar <span class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
