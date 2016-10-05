17ff67188921c2eb01bfbfe0de1c215d95681139
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2016-10-05 10:11:10 -0500

    SubModulo Tarifas

--- app/Modelos/Tarifas/Area.php
+++ app/Modelos/Tarifas/Area.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     protected $table = 'area';
-    protected $primaryKey = '';
+    protected $primaryKey = 'idarea';
     public $incrementing = false;
     public $timestamps = false;
 
