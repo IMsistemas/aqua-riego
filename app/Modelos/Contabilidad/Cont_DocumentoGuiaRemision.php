9716e68dc19ba76988732776aeadc8aee132f112
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2017-01-18 04:31:24 -0500

    Modelos para Modulo Guia de Remision

---
+++ app/Modelos/Contabilidad/Cont_DocumentoGuiaRemision.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Modelos\Contabilidad;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cont_DocumentoGuiaRemision extends Model
+{
+
+    protected $table = 'cont_documentoguiaremision';
+    protected $primaryKey = 'iddocumentoguiaremision';
+    public $timestamps = false;
+
+}
---
+++ app/Modelos/Contabilidad/Cont_DocumentoGuiaRemisionMerc.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Modelos\Contabilidad;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cont_DocumentoGuiaRemisionMerc extends Model
+{
+
+    protected $table = 'cont_documentoguiaremisionmerc';
+    protected $primaryKey = 'iddocumentoguiaremisionmerc';
+    public $timestamps = false;
+
+}
