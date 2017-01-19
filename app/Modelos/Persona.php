39a01fa5c84772c0d168f261b014adf79a1d79ad
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2017-01-19 08:11:56 -0500

    Refactoring Persona

--- app/Modelos/Persona.php
+++ app/Modelos/Persona.php
@@ -10,14 +10,7 @@ class Persona extends Model
     protected $table = 'persona';
     protected $primaryKey = 'idpersona';
     public $timestamps = false;
-    public $incrementing = false;
-    protected $fillable = [
-        'idtransportista',
-        'idpersona',
-        'razonsocial',
-        'placa',
-        'estado',        
-    ];
+
     public function transportista(){
     	return $this->hasMany('App\Modelos\Transportista');
     }
