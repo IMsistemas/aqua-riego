5462f20e967fd0f8e6c92756b72a9b25f4acdd63
dyero <dayi.yero90@gmail.com>
2017-02-06 21:36:56 -0500

    Configuracion Sistema

--- resources/views/ConfiguracionSystem/index.php
+++ resources/views/ConfiguracionSystem/index.php
@@ -19,7 +19,65 @@
 <body>
     <div ng-controller="configuracionsystemController">
 
+        <div class="container-fluid">
+            <div id="dvTab" style="margin-top: 5px;">
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active tabs"><a href="#empresa" aria-controls="empresa" role="tab" data-toggle="tab"> Empresa</a></li>
+                    <li role="presentation" class="tabs"><a href="#contabilidad" aria-controls="contabilidad" role="tab" data-toggle="tab"> Contabilidad</a></li>
+                    <li role="presentation" class="tabs"><a href="#sri" aria-controls="sri" role="tab" data-toggle="tab"> SRI</a></li>
+                    <li role="presentation" class="tabs"><a href="#especifica" aria-controls="especifica" role="tab" data-toggle="tab"> Especificas</a></li>
+                <div class="tab-content">
 
+                    <div role="tabpanel" class="tab-pane fade active in" id="empresa" style="padding-top: 10px;">
+
+                    </div>
+
+                    <div role="tabpanel" class="tab-pane fade" id="contabilidad">
+
+                        <div id="dvTab2" style="margin-top: 5px;">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li role="presentation" class="active tabs"><a href="#cont_general" aria-controls="cont_general" role="tab" data-toggle="tab"><i class="fa fa-info-circle" style="font-size: 20px !important;"></i> General</a></li>
+                                <li role="presentation" class="tabs"><a href="#cont_compras" aria-controls="cont_compras" role="tab" data-toggle="tab"><i class="fa fa-user" style="font-size: 20px !important;"></i> Compra</a></li>
+                                <li role="presentation" class="tabs"><a href="#cont_venta" aria-controls="cont_venta" role="tab" data-toggle="tab"><i class="fa fa-user" style="font-size: 20px !important;"></i> Ventas</a></li>
+                                <li role="presentation" class="tabs"><a href="#cont_notacredit" aria-controls="cont_notacredit" role="tab" data-toggle="tab"><i class="fa fa-user" style="font-size: 20px !important;"></i> Notas de Credito</a></li>
+                            </ul>
+                            <!-- Tab panels -->
+                            <div class="tab-content" style="padding-top: 10px;">
+                                <div role="tabpanel" class="tab-pane fade active in" id="cont_general">
+                                        a
+                                </div>
+
+                                <div role="tabpanel" class="tab-pane fade" id="cont_compras">
+                                        b
+                                </div>
+
+                                <div role="tabpanel" class="tab-pane fade" id="cont_venta">
+                                        c
+                                </div>
+
+                                <div role="tabpanel" class="tab-pane fade" id="cont_notacredit">
+                                        d
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                    <div role="tabpanel" class="tab-pane fade" id="sri" style="padding-top: 10px;">
+
+                    </div>
+
+                    <div role="tabpanel" class="tab-pane fade" id="especifica" style="padding-top: 10px;">
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
 
     </div>
 </body>
